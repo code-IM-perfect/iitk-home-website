943f4d07df01038b1c6c3aa0109a73d511d5729b
Harshit Tomar <harshitt345@gmail.com>
2023-12-30 21:32:47 +0530

    IDK I'm too lazy to commit more finely

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Website for IITK Home
+
+can be accessed at [home.iitk.ac.in/~harshitt23](https://home.iitk.ac.in/~harshitt23)
--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,32 @@ let smoother = ScrollSmoother.create({
 	smooth: 2,
 	effects: true,
 	normalizeScroll: true
-  });
+});
 
-  gsap.to("#heroHiExtended", {
+const cssVar = function (variable) {
+	return getComputedStyle(document.documentElement).getPropertyValue(variable)
+}
+
+gsap.to("#hero h1", {
 	scrollTrigger: {
-		trigger: "h1",
+		trigger: "#hero h1",
 		// markers: true,
 		scrub: true,
 		start: "center 50%",
-		end: "+=40%"
+		end: "+=47%"
 	},
-	duration: 2,
-	text: "iiiiiii!!!!!!!!!!!!!!!!!!!!!!",
+	text: "Hiiiiiiiii!!!!!!!!!!!!!!!!!!!!!!",
+	type: "diff",
+	// letterSpacing: "3rem",
 	ease: "power2.in"
 });
+
+gsap.to("#section1 span.harshit", {
+	scrollTrigger: {
+		trigger: "#section1 h2",
+		scrub: true,
+		start: "bottom 90%",
+		end: "bottom center"
+	},
+	color: cssVar("--catppuccin-mocha-red")
+})
